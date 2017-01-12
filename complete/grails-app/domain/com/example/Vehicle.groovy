e372a7d964672b86e5d3bc225ec3462e337086d3
Zachary Klein <zak@silver-chalice.com>
2017-01-11 22:04:18 -0600

    Added services section

--- complete/grails-app/domain/com/example/Vehicle.groovy
+++ complete/grails-app/domain/com/example/Vehicle.groovy
@@ -2,11 +2,14 @@ package com.example
 
 class Vehicle {
 
+    Integer year
+
     String name
     Model model
     Make make
 
     static constraints = {
+        year min: 1900
         name maxSize: 255
     }
 }
---
+++ complete/grails-app/services/com/example/ValueEstimateService.groovy
@@ -0,0 +1,11 @@
+package com.example
+
+import grails.transaction.Transactional
+
+@Transactional
+class ValueEstimateService {
+
+    def serviceMethod() {
+
+    }
+}
