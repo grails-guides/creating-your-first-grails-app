dd7421fed4b789f5ebebc03269c93ef326177ec5
Zachary Klein <zak@silver-chalice.com>
2017-06-27 21:15:59 -0500

    Fixed codenarc violations

--- complete/grails-app/controllers/org/grails/guides/UrlMappings.groovy
+++ complete/grails-app/controllers/org/grails/guides/UrlMappings.groovy
@@ -3,7 +3,7 @@ package org.grails.guides
 class UrlMappings {
 
     static mappings = {
-        "/$controller/$action?/$id?(.$format)?"{
+        "/$controller/$action?/$id?(.$format)?" {
             constraints {
                 // apply constraints here
             }
