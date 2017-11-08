3bfbca460ad6fe13bfe175e99a59ff48a3788aef
Sergio del Amo <sergio.delamo@softamo.com>
2017-11-08 15:50:52 +0100

    Add republish-guides-website to travis-build

--- travis-build.sh
+++ travis-build.sh
@@ -8,4 +8,14 @@ chmod 777 build-guide
 
 ./build-guide || EXIT_STATUS=$?
 
+if [[ $EXIT_STATUS -ne 0 ]]; then
+    echo "Build failed" 
+    exit $EXIT_STATUS
+fi
+
+curl -O https://raw.githubusercontent.com/grails/grails-guides/master/travis/republish-guides-website.sh
+chmod 777 republish-guides-website.sh
+
+./republish-guides-website.sh || EXIT_STATUS=$?
+
 exit $EXIT_STATUS
