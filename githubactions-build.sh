9b2e945023506006f77951a36315e80cad6712dc
Sergio del Amo <sergio.delamo@softamo.com>
2021-10-14 16:53:02 +0200

    replace travis with github actions

--- githubactions-build.sh
+++ githubactions-build.sh
@@ -3,17 +3,17 @@ set -e
 
 export EXIT_STATUS=0
 
-curl -O https://raw.githubusercontent.com/grails/grails-guides/master/travis/build-guide
+curl -O https://raw.githubusercontent.com/grails/grails-guides/master/githubactions/build-guide
 chmod 777 build-guide
 
 ./build-guide || EXIT_STATUS=$?
 
 if [[ $EXIT_STATUS -ne 0 ]]; then
-    echo "Build failed" 
+    echo "build-guide failed" 
     exit $EXIT_STATUS
-fi
+fi  
 
-curl -O https://raw.githubusercontent.com/grails/grails-guides/master/travis/republish-guides-website.sh
+curl -O https://raw.githubusercontent.com/grails/grails-guides/master/githubactions/republish-guides-website.sh
 chmod 777 republish-guides-website.sh
 
 ./republish-guides-website.sh || EXIT_STATUS=$?
